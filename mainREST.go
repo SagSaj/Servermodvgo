3a3a5238ba255e04f4f91f6365a0ca235fcb4de8
Stanislav Bartoshevich <Stas@MBP-Stanislav.lan>
2018-10-31 16:05:31 +0300

    initial version

---
+++ mainREST.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	re "REST"
+	"fmt"
+	"os"
+)
+
+//export
+const (
+	port = ":50051"
+)
+
+//var s mongo.SessionGame
+func main() {
+	//mongo.InitiateSession()
+	go re.GoServerListen()
+	var guessColor string
+	for {
+		if _, err := fmt.Scanf("%s", &guessColor); err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		if "exit" == guessColor {
+			os.Exit(0)
+			return
+		}
+	}
+}
